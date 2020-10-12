32840281e22546d026a4d027cb965cb54d58cd7a
Branimir Kunstek <branekuna@gmail.com>
2020-10-12 20:15:53 +0200

    added last project

--- Project Two - fitness tracker/graph.js
+++ Project Two - fitness tracker/graph.js
@@ -39,6 +39,26 @@ const yAxisGroup = graph.append('g').attr('class', 'y-axis');
 
 const path = graph.append('path');
 
+//create dotted line group and append to graph
+const dottedLines = graph
+  .append('g')
+  .attr('class', 'lines')
+  .style('opacity', 0);
+
+//create x dotted line and append to dotted line group
+const xDottedLine = dottedLines
+  .append('line')
+  .attr('stroke', '#aaa')
+  .attr('stroke-width', 1)
+  .attr('stroke-dasharray', 4);
+
+//create y dotted line and append to dotted line group
+const yDottedLine = dottedLines
+  .append('line')
+  .attr('stroke', '#aaa')
+  .attr('stroke-width', 1)
+  .attr('stroke-dasharray', 4);
+
 //data and firestore
 const update = (data) => {
   data = data.filter((item) => item.activity == activity);
@@ -78,6 +98,41 @@ const update = (data) => {
     .attr('cx', (d) => x(new Date(d.date)))
     .attr('cy', (d) => y(d.distance))
     .attr('fill', '#ccc');
+
+  //mouse enter/leave events for circles
+  graph
+    .selectAll('circle')
+    .on('mouseover', (e, d) => {
+      d3.select(e.currentTarget)
+        .transition()
+        .duration(100)
+        .attr('r', 8)
+        .attr('fill', '#fff');
+
+      //set x dotted line coordinates (x1, x2, y1, y2)
+      xDottedLine
+        .attr('x1', x(new Date(d.date)))
+        .attr('x2', x(new Date(d.date)))
+        .attr('y1', graphHeight)
+        .attr('y2', y(d.distance));
+      //set y dotted line coords (x1, x2, y1, y2)
+      yDottedLine
+        .attr('x1', 0)
+        .attr('x2', x(new Date(d.date)))
+        .attr('y1', y(d.distance))
+        .attr('y2', y(d.distance));
+      //show the dotted line group (.style, opacity)
+      dottedLines.style('opacity', 1);
+    })
+    .on('mouseout', (e, d) => {
+      d3.select(e.currentTarget)
+        .transition()
+        .duration(300)
+        .attr('r', 4)
+        .attr('fill', '#ccc');
+      //hide the dotted line group (.style, opacity)
+      dottedLines.style('opacity', 0);
+    });
 
   //create axes
   const xAxis = d3.axisBottom(x).ticks(4).tickFormat(d3.timeFormat('%b %d'));
